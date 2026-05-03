6728a195e551ac251787a654611f223b383cd7c6
Sam <dev.yosam@gmail.com>
2026-05-03 15:28:50 +1000

    add empty IELTSBuddy Xcode skeleton

---
+++ IELTSBuddy/IELTSBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  IELTSBuddyApp.swift
+//  IELTSBuddy
+//
+//  Created by yosam on 3/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct IELTSBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
