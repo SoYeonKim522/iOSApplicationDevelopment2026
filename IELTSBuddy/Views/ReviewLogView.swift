21e675d05532633df4ab1c2e995290b602ac8252
Stefy73 <thomastefy@gmail.com>
2026-05-08 10:40:18 +1000

    Add review log and history view

---
+++ IELTSBuddy/Views/ReviewLogView.swift
@@ -0,0 +1,18 @@
+//
+//  ReviewLogView.swift
+//  IELTSBuddy
+//
+//  Created by Stefy Thomas on 7/5/2026.
+//
+
+import SwiftUI
+
+struct ReviewLogView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReviewLogView()
+}
