d4af85507a5d4cdc6baf6f67ad5fa3abd2a97f85
Stefy73 <thomastefy@gmail.com>
2026-05-15 01:24:53 +1000

    Add storage keys

---
+++ IELTSBuddy/Utilities/StorageKeys.swift
@@ -0,0 +1,12 @@
+//
+//  StorageKeys.swift
+//  IELTSBuddy
+//
+//  Created by Stefy Thomas on 15/5/2026.
+//
+
+import Foundation
+enum StorageKeys {
+    static let savedSessions = "savedSessions"
+    static let bookmarkedMistakes = "bookmarkedMistakes"
+}
